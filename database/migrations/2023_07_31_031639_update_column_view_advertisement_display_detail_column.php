d6f8313a0def90a0af8938ed29b1746a21cc9c43
Aditya Suryadi <adityasuryaditya@gmail.com>
2023-07-31 10:39:11 +0700

    fix:update column view nullable

---
+++ database/migrations/2023_07_31_031639_update_column_view_advertisement_display_detail_column.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('advertisement_display_details', function (Blueprint $table) {
+            $table->integer('view')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertisement_display_details');
+    }
+};
